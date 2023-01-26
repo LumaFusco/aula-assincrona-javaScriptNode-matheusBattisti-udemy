01eb90524119f4771016e69002e312a7affa1a67
LumaFusco <lumafuscoconde@gmail.com>
2023-01-26 15:20:41 -0300

    exercicios-de-estrutur-de-programacao

---
+++ 5_exercicios_estruturas_de_programacao/index.js
@@ -0,0 +1,93 @@
+//EXERCÍCIO 6
+
+/* var nome = "Luma"
+console.log(nome);
+console.log(typeof (nome));
+
+
+const numero = 123;
+console.log(numero);
+console.log(typeof (numero));
+
+let alternativa = false;
+console.log(alternativa);
+console.log(typeof (alternativa)); */
+
+
+
+//EXERCÍCIO 7
+
+/* let idade = 18;
+
+if(idade >= 18){
+    console.log("Pode entrar!");
+} else {
+    console.log("Entrada negada!");
+} */
+
+
+
+//EXERCÍCIO 9
+
+/* const numero = 18;
+
+console.log(Math.pow(2,2));
+console.log(Math.pow(3,2));
+console.log(Math.pow(numero,2)); */
+
+
+
+//EXERCÍCIO 10
+
+/* var velocidade = 50;
+
+if (velocidade <= 80){
+console.log("Velocidade permitida");
+} else {
+    console.log("Velocidade acima do permitido");
+} */
+
+
+
+//EXERCÍCIO 11
+
+/* var idade = 21;
+var CNH = true;
+
+if (idade < 18 && CNH == false) {
+    console.log("Você não tem idade nem CNH para dirigir");
+} else if (idade > 18 && CNH == false) {
+    console.log("Você tem idade mas não tem CNH para dirigir");
+} else {
+    console.log("Você está habilitado a dirigir");
+}
+ */
+
+
+//EXERCÍCIO 12
+
+/* let number = 0;
+
+while (number = 50){
+    console.log(number);
+    number ++;
+}; */
+
+
+//EXERCÍCIO 13
+
+/* for (number = 100; number >= 50; number--){
+    console.log(number);
+} */
+
+
+//EXERCÍCIO 14
+
+/* for(let i = 0; i <= 50; i++) {
+
+    if(i % 2 == 0) {
+        console.log(`O numero ${i} é par`);
+    } else {
+        console.log(`O numero ${i} é impar`);
+    }
+} */
